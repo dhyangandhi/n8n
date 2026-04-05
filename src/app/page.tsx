a0700962f74497ca730c06295ab6d13530e5afb3
DhyanGandhi <dhyangandhi2008@gmail.com>
2026-04-05 09:02:06 +0530

    01: setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const Page = () => {
   return (
-    <div className="text-red-500 font-semibold">
-      Hello World!
+    <div className="min-h-screen flex items-center min-w-screen justify-center">
+      <Button variant="outline" size="lg">
+        Click me
+      </Button>
     </div>
   )
 }
